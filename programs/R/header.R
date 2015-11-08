cdf41ba94be962681cb2aa16ac9ae3958f19ea04
Pawel <pavopax@gmail.com>
2015-11-07 19:05:23 -0500

    add /programs/R directory

---
+++ programs/R/header.R
@@ -0,0 +1,14 @@
+## ============================================================================
+## initialization file for this project
+## Paull Paczuski [pavopax.com] [AMA!]
+## ============================================================================
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+## library(scales)
+## library(gridExtra)
+
+source("functions.R")
+
+
